a860b9e0ffdc91ff5a6ce1747665dc8e0c2ff1a2
anaalthoff <anapaula.althoff@gmail.com>
2025-09-19 04:33:40 -0300

    feat: cria classe Program com o método Main

--- EstudosEmCSharp/Program.cs
+++ EstudosEmCSharp/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EstudosEmCSharp;
+
+// A classe Program é a classe base
+// Todo programa em C# é em si uma classe
+public class Program
+{
+    // método público que não retorna nada
+    // Método Main é o método principal da classe
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
